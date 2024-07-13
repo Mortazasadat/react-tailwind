65edc9f2d5ee539aad7118662975b0cf5f52663e
Sayed Murtaza <mortazasadat37@gmail.com>
2024-07-13 22:30:07 +0430

    api

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GITHUB_API_KEY: process.env.NEXT_PUBLIC_GITHUB_API_KEY,
+  },
+};
 
 export default nextConfig;
