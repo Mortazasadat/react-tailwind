fce49afb9abc379459f217cac2be0fb7edb4d6c2
Sayed Murtaza <mortazasadat37@gmail.com>
2024-07-28 14:25:14 +0430

    site conf

---
+++ src/utils/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "React Tailwind",
+  description:
+    "React tailwind is a beautifull UI components and templates, it's free for creative developers.",
+  url: "https://react-tailwind.com/",
+  // ogImage: "",
+
+  links: {
+    twitter: "https://x.com/sayedmurtaza07",
+    github: "https://github.com/Mortazasadat/react-tailwind",
+    linkedin: "https://www.linkedin.com/in/sayedmurtaza/",
+  },
+  // icons: {
+  //   icon: [{ rel: "icon", url: "/favicon.ico" }],
+  //   shortcut: "/apple-touch-icon.png",
+  // },
+};
